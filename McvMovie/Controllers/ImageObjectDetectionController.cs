63fede7c46a9f4b1d6012ad613e5d79c67b6d82f
Uczeń ZSŁ <hubertwit2005@gmail.com>
2023-04-17 16:11:59 +0200

    Wysyanie obrazka do wyniku.cshtml

--- McvMovie/Controllers/ImageObjectDetectionController.cs
+++ McvMovie/Controllers/ImageObjectDetectionController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Text.Json;
 
 namespace McvMovie.Controllers
 {
     public class ImageObjectDetectionController : Controller
     {
+        private readonly string ApiUrl = "https://technikum01.cognitiveservices.azure.com";
+        public string ApiKey = "d0b1d44871564786934befe406045484";
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Detect(IFormFile inputFile)
+        {
+            var apiClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(ApiKey)) { Endpoint = ApiUrl };
+            DetectResult detectResults = await apiClient.DetectObjectsInStreamAsync(inputFile.OpenReadStream());
+            ViewBag.Message = JsonSerializer.Serialize(detectResults);
+
+            return View("Wynik");
         }
     }
 }
